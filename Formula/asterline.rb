9a6b82fab075610804ceab92fd339afb96d5f311
song0705 <pu20060705@163.com>
2026-08-14 17:21:21 +0800

    fix: infer Formula version from release URL

--- Formula/asterline.rb
+++ Formula/asterline.rb
@@ -1,19 +1,18 @@
 class Asterline < Formula
   desc "Local-first terminal workspace for coordinating coding agents"
   homepage "https://github.com/song0705/Asterline"
-  version "0.2.5"
   license "MIT"
 
   depends_on :macos
 
   on_macos do
     on_arm do
-      url "https://github.com/song0705/Asterline/releases/download/v#{version}/asterline-#{version}-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/song0705/Asterline/releases/download/v0.2.5/asterline-0.2.5-aarch64-apple-darwin.tar.gz"
       sha256 "9fe038680d0e3a380b4bbb190865c46b5b6dc4348314f067f8c0fdd732d957a6"
     end
 
     on_intel do
-      url "https://github.com/song0705/Asterline/releases/download/v#{version}/asterline-#{version}-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/song0705/Asterline/releases/download/v0.2.5/asterline-0.2.5-x86_64-apple-darwin.tar.gz"
       sha256 "1970b9ea724481ee54b49ea306aabb3cf7e81bcb09d6e5a6aee1cd80516d412f"
     end
   end
