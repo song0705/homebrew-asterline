8fae4112cf7b0ea51be2802b32849c384cb553b3
song0705 <pu20060705@163.com>
2026-08-15 21:23:51 +0800

    asterline 0.2.9

--- Formula/asterline.rb
+++ Formula/asterline.rb
@@ -5,25 +5,25 @@ class Asterline < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/song0705/Asterline/releases/download/v0.2.8/asterline-0.2.8-aarch64-apple-darwin.tar.gz"
-      sha256 "fe93fedc7fa561e6cfa9399cab9a09fa7fecd30df60773a25d0c2dad02cb024c"
+      url "https://github.com/song0705/Asterline/releases/download/v0.2.9/asterline-0.2.9-aarch64-apple-darwin.tar.gz"
+      sha256 "322604253de110254c119300d562c2646992465026ba85173acca82b08bcb6a4"
     end
 
     on_intel do
-      url "https://github.com/song0705/Asterline/releases/download/v0.2.8/asterline-0.2.8-x86_64-apple-darwin.tar.gz"
-      sha256 "be17f0575a98a8814ccdba9730b1067613d62b2fc68c27bea3b34b7ee0adc0e4"
+      url "https://github.com/song0705/Asterline/releases/download/v0.2.9/asterline-0.2.9-x86_64-apple-darwin.tar.gz"
+      sha256 "ed33fbc31705663e5a00cc03a53ebb522e0e3d0db6bb5286a3bc5e3b62162842"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/song0705/Asterline/releases/download/v0.2.8/asterline-v0.2.8-Linux-arm64.tar.gz"
-      sha256 "18cf85bcb1000040aa6231891cc169f47f19da913e033bb28a8f564f26bc33ae"
+      url "https://github.com/song0705/Asterline/releases/download/v0.2.9/asterline-v0.2.9-Linux-arm64.tar.gz"
+      sha256 "fb2afd79405f25ed697c596860a169599d6bb258e232f34fc5df6069a71de766"
     end
 
     on_intel do
-      url "https://github.com/song0705/Asterline/releases/download/v0.2.8/asterline-v0.2.8-Linux-x86_64.tar.gz"
-      sha256 "f8a35ede4a74b3e8158708369f06ee1c44708204652e23399acd76b319c2e164"
+      url "https://github.com/song0705/Asterline/releases/download/v0.2.9/asterline-v0.2.9-Linux-x86_64.tar.gz"
+      sha256 "eedde381531acfbad4a451cbdd8c5de008b4f54d6f0f42567742ae90451b802d"
     end
   end
 
